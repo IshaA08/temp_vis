81989c378555d93cdbf59f1407ff6b67eabf1694
Isha Afzaal <25419186+IshaA08@users.noreply.github.com>
2024-08-03 20:36:06 -0600

    Extract and plot corresponding date information

--- calgary_highs.py
+++ calgary_highs.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 # Check if a given string is a number or not
@@ -24,24 +25,28 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
-    # Get the daily max temps
-    highs = []
+    # Get the daily max temps and corresponding dates
+    dates, highs = [], []
     for row in reader:
         if not is_number(row[3]):
             continue
+        current_date = datetime.strptime(row[4][0:10], '%Y-%m-%d')
         high = float(row[3])
+        dates.append(current_date)
         highs.append(high)
 
+print(dates)
 print(highs)
 
 # Plot the extracted max temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Format the plot
 plt.title("Daily high temperatures, June-July 2024", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (C)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
